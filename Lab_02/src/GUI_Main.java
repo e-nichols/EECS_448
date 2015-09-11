96856ec31ded49dd2d6eecd820eb1ec310a17f8b
Evan Nichols <evan.t.nichols@gmail.com>
2015-09-11 12:05:43 -0500

    finished GUI, added ActionListener

--- Lab_02/src/GUI_Main.java
+++ Lab_02/src/GUI_Main.java
@@ -1,10 +1,16 @@
 import javax.swing.*;
 import java.awt.event.*;
 
-public class GUI_Main extends JFrame {
+public class GUI_Main extends JFrame implements ActionListener {
+	
+	//global declare of result and input fields
+	JTextField resultField = new JTextField( 10 );
+	JTextField input1 = new JTextField( 10 );
+	JTextField input2 = new JTextField( 10 );
 	
 	public static void main(String[] args)
 	{
+		
 		//create instance of GUI_Main
 		GUI_Main myGUI = new GUI_Main();
 		
@@ -14,19 +20,17 @@ public class GUI_Main extends JFrame {
 	public GUI_Main () {
 				
 		JPanel panel = new JPanel();
+		setTitle( "Calculator App" );
 		
 		panel.setBorder( BorderFactory.createEmptyBorder( 10, 10, 10, 10 ) );
 		
-		//create the input text fields
+		//create the input titles
 		JLabel title1 = new JLabel( "Value 1" );
-		JTextField input1 = new JTextField( 200 );
-		
 		JLabel title2 = new JLabel( "Value 2" );
-		JTextField input2 = new JTextField( 200 );
 		
 		//create output title and field
 		JLabel resultTitle = new JLabel( "Result" );
-		JTextField result = new JTextField( 200 );
+		resultField.setEditable( false );
 		
 		//add them to screen
 		panel.add( title1 );
@@ -37,19 +41,52 @@ public class GUI_Main extends JFrame {
 		//add button
 		JButton addButton = new JButton( "Add" );
 		panel.add( addButton );
+		addButton.addActionListener( this );
 		
 		panel.add( resultTitle );
-		panel.add( result );
-		
-
+		panel.add( resultField );
 		
 		add( panel );
+		setVisible( true );	
+		setSize( 500, 500 );
 		
-		setTitle( "Calculator App" );
-		setSize( 400, 400 );
-		setVisible( true );
 		setDefaultCloseOperation( EXIT_ON_CLOSE );
 		
+	}
+	
+	//implement Action Methods
+	public void actionPerformed( ActionEvent e) 
+	{
+		int val1, val2;
+		
+		String string1 = input1.getText();
+		String string2 = input2.getText();
+		
+		//handles case of user only inputting value in one or none of the input fields
+		if( string1.length() == 0 ) 
+		{
+			 val1 = 0;
+		}
+		else
+		{
+			val1 = Integer.parseInt(string1);
+		}
+		
+		if( string2.length() == 0 ) 
+		{
+			 val2 = 0;
+		}
+		else
+		{
+			val2 = Integer.parseInt(string2);
+		}
+		
+		//calculate!
+		int result = val1+val2;
+		
+		//set result to String and send to resultField
+		String resultString = Integer.toString(result);
+		resultField.setText(resultString);
 	}
 
 }
